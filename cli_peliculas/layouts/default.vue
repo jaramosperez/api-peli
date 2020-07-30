bbfa9cd162c8eab3e9aed0760cf05b21e994746f
Javier Ramos <jaramosperez85@gmail.com>
2020-07-30 17:08:36 -0400

    Creando el cliente y el componente Peli

--- cli_peliculas/layouts/default.vue
+++ cli_peliculas/layouts/default.vue
@@ -6,44 +6,50 @@
       aria-label="main navigation"
     >
       <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
-          >
-        </a>
 
-        <div class="navbar-burger">
+        <a class="navbar-item" href="/">Super Películas</a>
+
+        <div class="navbar-burger" @click="isActive = !isActive">
           <span />
           <span />
           <span />
         </div>
       </div>
+      <!-- Contenido del menú, al nivel del navbar-brand-->
+      <div class="navbar-menu" :class="{ active: isActive }">
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button">
+                <strong>Registro</strong>
+              </a>
+              <a class="button is-light">
+                Acceder
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
 
     <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
+<aside class="column is-3 section"> <!-- 3 en lugar de 2 y 9 por 10 -->
+  <p class="menu-label is-hidden-touch">General</p>
+  <ul class="menu-list">
+    <li>
+      <nuxt-link to="/" exact-active-class="is-active">
+        <b-icon icon="video"/>
+        Películas
+      </nuxt-link>
+    </li>
+    <li>
+      <nuxt-link to="/favoritas" exact-active-class="is-active">
+        <b-icon icon="star"/>
+        Favoritas
+      </nuxt-link>
+    </li>
+  </ul>
+</aside>
 
       <div class="container column is-10">
         <nuxt />
@@ -56,19 +62,13 @@
 export default {
   data () {
     return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
-    }
+      isActive: false
+    };
   }
 }
 </script>
+<style>
+.active{
+  display: block !important;
+}
+</style>
--- cli_peliculas/pages/index.vue
+++ cli_peliculas/pages/index.vue
@@ -1,53 +1,51 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
-    </div>
-  </section>
+ <section class="section">
+   <h2 class="title">Películas</h2>
+   <div class="columns is-mobile is-multiline">
+        <Peli v-for="peli in pelis" :key="peli.id" :peli="peli"/>
+     </peli>
+   </div>
+ </section>
 </template>
 
 <script>
-import Card from '~/components/Card'
+import Peli from '~/components/Peli'
 
 export default {
   name: 'HomePage',
 
   components: {
-    Card
-  }
+    Peli
+  },
+  data: function () {
+   return {
+     'pelis': [
+       {
+         'id': 1,
+         'titulo': 'El Padrino',
+         'favoritos': '3',
+         'imagen': 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg',
+         'estreno': '1972',
+         'resumen': 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+       },
+       {
+         'id': 2,
+         'titulo': 'El Padrino: Parte 2',
+         'favoritos': '1',
+         'imagen': 'https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg',
+         'estreno': '1974',
+         'resumen': 'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.',
+       },
+       {
+         'id': 3,
+         'titulo': 'El Padrino: Parte 3',
+         'favoritos': '0',
+         'imagen': 'https://m.media-amazon.com/images/M/MV5BNTc1YjhiNzktMjEyNS00YmNhLWExYjItZDhkNWJjZjYxOWZiXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+         'estreno': '1990',
+         'resumen': 'In the midst of trying to legitimize his business dealings in New York City and Italy in 1979, aging Mafia Don Michael Corleone seeks to avow for his sins, while taking his nephew Vincent Mancini under his wing.',
+       }
+     ]
+   }
+ },
 }
 </script>
